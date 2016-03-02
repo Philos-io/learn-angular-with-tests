316dbe0482121e7e39e52f261091332141584249
davyengone <engonenzedavy@yahoo.com>
2016-03-02 08:17:23 +0100

    added more tests for bookfactory

--- test/bookfactory.spec.js
+++ test/bookfactory.spec.js
@@ -32,7 +32,24 @@ describe('BookFactory', () => {
     $httpBackend = $injector.get('$httpBackend');
 
     let response = {
-      data: ['JavaScript', 'Angular']
+      data: [
+        {
+          id: 8988,
+          author: 'M. Henry',
+          title: 'JavaScript Rocks!',
+          price: 12,
+          description: 'JavaScript Rocks really!',
+          url: 'src/assets/images/javascript.png'
+        },
+        {
+          id: 8976,
+          author: 'M. Martine',
+          title: 'React Rocks!',
+          price: 15,
+          description: 'React Rocks really!',
+          url: 'src/assets/images/react.png'
+        }
+      ]
     };
 
     $httpBackend.expectGET(url).respond(response);
@@ -53,6 +70,23 @@ describe('BookFactory', () => {
           expect(response.data.length).toEqual(2);
         });
       $httpBackend.flush();
+    });
+
+    describe('Book', ()=>{
+      it('Should have an id, description, title, author, price and url', ()=>{
+        bookFactory.getBooks()
+          .then((response)=>{
+            let book = response.data[0];
+            expect(book.id).toBeDefined();
+            expect(book.author).toBeDefined();
+            expect(book.title).toBeDefined();
+            expect(book.price).toBeDefined();
+            expect(book.description).toBeDefined();
+            expect(book.url).toBeDefined();
+          });
+
+        $httpBackend.flush();
+      });
     });
   });
 });
