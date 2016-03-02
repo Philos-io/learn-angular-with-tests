c7f83f495a804293346627f3e4cb353243e507b8
davyengone <engonenzedavy@yahoo.com>
2016-03-02 08:16:33 +0100

    added babel to wallaby

--- wallaby.js
+++ wallaby.js
@@ -19,32 +19,8 @@ module.exports = function(wallaby){
     tests: [
       'test/*.spec.js'
     ],
-    preprocessors: {
-      'templates/*.html': function(file){
-        return require('wallaby-ng-html2js-preprocessor').transform(file, {
-          // strip this from the file path
-          stripPrefix: 'public/',
-          stripSufix: '.ext',
-          // prepend this to the
-          prependPrefix: 'served/',
-          // setting this option will create only a single module that contains templates
-          // from all the files, so you can load them all with module('foo')
-          // moduleName: 'templates'
-        });
-      }
+    compilers: {
+      'test/*.spec.js': babelCompiler
     }
   };
 };
-
-
-
-// // strip this from the file path
-// stripPrefix: 'public/',
-// stripSufix: '.ext.js',
-// // prepend this to the
-// prependPrefix: 'serve/',
-//
-// // or define a custom transform function
-// cacheIdFromPath: function(filepath) {
-// return cacheId;
-// },
